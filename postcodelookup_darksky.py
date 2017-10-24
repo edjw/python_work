a91f21c67614956269b516f4c24099bdce4083be
personal <Personal@eds-macbook-air.home>
2017-10-24 10:13:17 +0100

    added description, to-dos, what I learnt

--- postcodelookup_darksky.py
+++ postcodelookup_darksky.py
@@ -1,3 +1,15 @@
+""" Asks for a UK postcode, uses postcodes.io to get the latitude and longitude of that postcode, rounds the result to 4 decimal places as that's the most Dark Sky uses, and opens darksky.net to get the forecast for the longitude/latitude of that postcode. """
+
+"""
+todo
+1. Use regex or postcode.io's postcode validation to check the postcode entered is a valid UK postcode and tell the user to re-enter a proper postcode
+2. use darksky api and do something else with this info
+"""
+
+"""
+Learnt: requests, using an API, returning values from a function, parsing JSON in python, input prompts, confidence!
+"""
+
 import requests, webbrowser
 
 postcode = input('Enter your postcode to check the weather on DarkSky.\n')
@@ -19,5 +31,4 @@ openDarkSky(latitude_longitude)
 
 
 
-#append latitude_longitude to darksky url and open it
-#or use darksky api and do something with that
+
