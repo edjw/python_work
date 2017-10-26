9a53c8c889de7d0feb414c121c1e935aff79ced8
personal <Personal@eds-macbook-air.home>
2017-10-26 06:53:17 +0100

    Made variable names in getURLs function more generic and therefore reusable

--- scrape_multiple_urls.py
+++ scrape_multiple_urls.py
@@ -5,22 +5,22 @@ import requests, bs4
 
 def getURLs():
     """Gets the user to enter one or more URLs to be scraped, very imperfectly checks whether a URL was entered"""
-    urls = [] #Using a list to allow multiple URLs to be scraped at once
+    savedUserInput = [] #Using a list to allow multiple URLs to be scraped at once
     userInput = None
-    while not urls: #When no URL has been entered, ask the user to enter a URL
+    while not savedUserInput: #When no URL has been entered, ask the user to enter a URL
         userInput = input("Enter a full URL (starting with http:// or https://) to get the HTML for that webpage.\n")
-        while userInput.startswith('http') == False: #Imperfectly checks whether the user entered a full URL and tells the user to enter a full URL if they didn't
+        while savedUserInput.startswith('http') == False: #Imperfectly checks whether the user entered a full URL and tells the user to enter a full URL if they didn't
             userInput = input("That wasn't a URL. Enter a full URL to get the HTML for that webpage.\n")
-        urls.append(userInput) #Save the URL to be scraped later
+        savedUserInput.append(userInput) #Save the URL to be scraped later
 
-    while urls: #Once at least one URL has been entered, ask if the user's done or wants to scrape more URLs
+    while savedUserInput: #Once at least one URL has been entered, ask if the user's done or wants to scrape more URLs
         userInput = input("Great. Enter another URL or just press Return if you're finished.\n")
         if len(userInput) == 0: #If nothing was entered, then the user just pressed Return so they've finished entering URLs. Skip to the end of the function
             break
         else: 
             while userInput.startswith('http') == False: #Imperfectly checks whether the user entered a full URL and tells the user to enter a full URL if they didn't
                 userInput = input("That wasn't a URL. Enter a full URL or just press Return if you're finished.\n")
-            urls.append(userInput) #Save the URL to be scraped later
+            savedUserInput.append(userInput) #Save the URL to be scraped later
     return urls
 urls = getURLs()
 
